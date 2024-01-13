c9040bdd56f06dd5b663ea9e22265d4777714321
daichi <aryadavicenna@gmail.com>
2024-01-13 13:57:30 +0700

    fix screen overflow

--- lib/pages/detail/web_detail_page.dart
+++ lib/pages/detail/web_detail_page.dart
@@ -20,122 +20,120 @@ class _WebDetailPage extends State<WebDetailPage> {
     final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: kIsWeb ? null : AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 48.0),
-        child: Center(
-          child: SizedBox(
-            width: screenWidth <= 1200 ? 800 : 1200,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  widget.book.title,
-                  style: const TextStyle(
-                      fontSize: 24.0, fontWeight: FontWeight.w700),
-                ),
-                const SizedBox(height: 32.0),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                        flex: 1,
-                        child: Column(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(16.0),
-                              child: Image.asset(widget.book.imageUrl),
-                            ),
-                            const SizedBox(height: 16.0),
-                          ],
-                        )),
-                    Expanded(
-                        flex: 2,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 64.0),
+          child: Center(
+            child: SizedBox(
+              width: screenWidth <= 1200 ? 800 : 1200,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                          flex: 1,
                           child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  widget.book.title,
-                                  style: const TextStyle(
-                                      fontSize: 28.0,
-                                      fontWeight: FontWeight.w700),
-                                ),
-                                const SizedBox(height: 8.0),
-                                Text(
-                                  widget.book.author,
-                                  style: const TextStyle(
-                                      fontSize: 20.0,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                                const SizedBox(height: 16.0),
-                                Text(
-                                  'Genre: ${widget.book.genre.join(', ')}',
-                                  style: const TextStyle(
-                                      fontSize: 16.0,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                                const SizedBox(height: 16.0),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: <Widget>[
-                                    Column(
-                                      children: <Widget>[
-                                        const Icon(Icons.groups,
-                                            color: Colors.blue),
-                                        const SizedBox(height: 10.0),
-                                        Text(
-                                          'Reviews: ${widget.book.totalReviews}',
-                                          style: const TextStyle(
-                                              fontSize: 16.0,
-                                              fontWeight: FontWeight.w600),
-                                        )
-                                      ],
-                                    ),
-                                    Column(
-                                      children: <Widget>[
-                                        const Icon(Icons.star_half,
-                                            color: Colors.orange),
-                                        const SizedBox(height: 10.0),
-                                        Text(
-                                          'Rating: ${widget.book.rating}',
-                                          style: const TextStyle(
-                                              fontSize: 16.0,
-                                              fontWeight: FontWeight.w600),
-                                        )
-                                      ],
-                                    ),
-                                    Column(
-                                      children: <Widget>[
-                                        const Icon(Icons.attach_money,
-                                            color: Colors.green),
-                                        const SizedBox(height: 10.0),
-                                        Text(
-                                          'USD: ${widget.book.price}',
-                                          style: const TextStyle(
-                                              fontSize: 16.0,
-                                              fontWeight: FontWeight.w600),
-                                        )
-                                      ],
-                                    ),
-                                    const Column(
-                                      children: [FavoriteButton()],
-                                    )
-                                  ],
-                                ),
-                                const SizedBox(height: 16.0),
-                                Text(
-                                  widget.book.description,
-                                  style: const TextStyle(
-                                      fontSize: 16.0,
-                                      fontWeight: FontWeight.w400),
-                                ),
-                              ]),
-                        ))
-                  ],
-                )
-              ],
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(16.0),
+                                child: Image.asset(widget.book.imageUrl),
+                              ),
+                              const SizedBox(height: 16.0),
+                            ],
+                          )),
+                      Expanded(
+                          flex: 3,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    widget.book.title,
+                                    style: const TextStyle(
+                                        fontSize: 28.0,
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                  const SizedBox(height: 8.0),
+                                  Text(
+                                    widget.book.author,
+                                    style: const TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(height: 16.0),
+                                  Text(
+                                    'Genre: ${widget.book.genre.join(', ')}',
+                                    style: const TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(height: 16.0),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: <Widget>[
+                                      Column(
+                                        children: <Widget>[
+                                          const Icon(Icons.groups,
+                                              color: Colors.blue),
+                                          const SizedBox(height: 10.0),
+                                          Text(
+                                            'Reviews: ${widget.book.totalReviews}',
+                                            style: const TextStyle(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.w600),
+                                          )
+                                        ],
+                                      ),
+                                      Column(
+                                        children: <Widget>[
+                                          const Icon(Icons.star_half,
+                                              color: Colors.orange),
+                                          const SizedBox(height: 10.0),
+                                          Text(
+                                            'Rating: ${widget.book.rating}',
+                                            style: const TextStyle(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.w600),
+                                          )
+                                        ],
+                                      ),
+                                      Column(
+                                        children: <Widget>[
+                                          const Icon(Icons.attach_money,
+                                              color: Colors.green),
+                                          const SizedBox(height: 10.0),
+                                          Text(
+                                            'USD: ${widget.book.price}',
+                                            style: const TextStyle(
+                                                fontSize: 16.0,
+                                                fontWeight: FontWeight.w600),
+                                          )
+                                        ],
+                                      ),
+                                      const Column(
+                                        children: [FavoriteButton()],
+                                      )
+                                    ],
+                                  ),
+                                  const SizedBox(height: 16.0),
+                                  Text(
+                                    widget.book.description,
+                                    style: const TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                ]),
+                          ))
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
